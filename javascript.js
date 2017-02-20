13143911bee73b616e49747d7d1ba9729a2b257b
Rasmus Nilsson <zawroli@hotmail.com>
2017-02-20 15:02:42 +0100

    Ny design igen

--- javascript.js
+++ javascript.js
@@ -95,10 +95,10 @@ function newStatement() { //spottar ut ett random statement som inte har använd
 	usedStatements.push(statementIndex);
 	lastStatement = statementIndex;
 	var newStatement = statement[statementIndex];
-	$(".middle > p").fadeTo(200, 0, function() {
-		$(".middle > p").text(newStatement); //printar ut det nya uttrycket
+	$(".secondScreen p").fadeTo(200, 0, function() {
+		$(".secondScreen p").text(newStatement); //printar ut det nya uttrycket
 	});
-	$(".middle > p").fadeTo(200, 1);
+	$(".secondScreen p").fadeTo(200, 1);
 }
 
 newStatement();
