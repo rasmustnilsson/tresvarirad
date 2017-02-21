6e0a47e051ed0539c9796bc164763dac9aa3c12b
Rasmus Nilsson <zawroli@hotmail.com>
2017-02-21 14:55:44 +0100

    Fixade bugg med ripple effekten

--- javascript.js
+++ javascript.js
@@ -201,30 +201,25 @@ function progressBar() {
 }
 
 var parent, ink, d, x, y;
-$(".secondScreen span button").click(function(e){
-	parent = $(this).parent();
-	console.log(parent);
-	//create .ink element if it doesn't exist
-	if(parent.find(".ink").length == 0)
-		parent.prepend("<span class='ink'></span>");
-		
-	ink = parent.find(".ink");
-	//incase of quick double clicks stop the previous animation
-	ink.removeClass("animate");
-	
-	//set size of .ink
-	if(!ink.height() && !ink.width())
-	{
-		//use parent's width or height whichever is larger for the diameter to make a circle which can cover the entire element.
-		d = Math.max(parent.outerWidth(), parent.outerHeight());
-		ink.css({height: d, width: d});
-	}
-	
-	//get click coordinates
-	//logic = click coordinates relative to page - parent's position relative to page - half of self height/width to make it controllable from the center;
-	x = e.pageX - parent.offset().left - ink.width()/2;
-	y = e.pageY - parent.offset().top - ink.height()/2;
-	
-	//set the position and add class .animate
-	ink.css({top: y+'px', left: x+'px'}).addClass("animate");
-})
+
+$(function(){
+	var ink, d, x, y;
+	$(".secondScreen span button").click(function(e){
+    if($(this).find(".ink").length === 0){
+        $(this).prepend("<span class='ink'></span>");
+    }
+         
+    ink = $(this).find(".ink");
+    ink.removeClass("animate");
+     
+    if(!ink.height() && !ink.width()){
+        d = Math.max($(this).outerWidth(), $(this).outerHeight());
+        ink.css({height: d, width: d});
+    }
+     
+    x = e.pageX - $(this).offset().left - ink.width()/2;
+    y = e.pageY - $(this).offset().top - ink.height()/2;
+     
+    ink.css({top: y+'px', left: x+'px'}).addClass("animate");
+});
+});
